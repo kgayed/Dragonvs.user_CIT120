78c33fb9c83f2fa35ce3b9e332e38d9c0e4c6232
kgayed <89406007+kgayed@users.noreply.github.com>
2021-09-26 03:13:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var ele=document.body.querySelector(".dragon");
 var userPoints =0;
 var dragonPoints =0;
 
-var hits=Number(prompt("What is the number of hits you are going to hit the dragon with: "));
-while (hits > 5) {
-    hits=Number(prompt("Enter a number up to 5: "));
-}
+var hits=Number(prompt("What is the number of hits you are going to hit the dragon with up to 5: "));
+
 var userNumber= Number(prompt("Enter a number that determine the damage to cause to the dragon: "));
 
 var i = 1;
